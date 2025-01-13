32e6bf13b93b1ce790750b01d1d6e5a65003741c
Aftersol <42478428+Aftersol@users.noreply.github.com>
2025-01-12 21:28:05 -0800

    updated notice for this single header library
    includes license notice and minor changes to other parts of the notice

--- inc/sQOI.h
+++ inc/sQOI.h
@@ -20,7 +20,9 @@
 
     THE QUITE OK IMAGE FORMAT
     Implementation based on the Version 1.0 created by Dominic Szablewski
-    Published on 2022.01.05 – qoiformat.org - https://qoiformat.org/qoi-specification.pdf
+    Published on 2022.01.05 – qoiformat.org
+    
+    QOI specification - https://qoiformat.org/qoi-specification.pdf
 
     Reference QOI implementation - https://github.com/phoboslab/qoi
 
@@ -40,11 +42,38 @@
 
 /*
 
-    Define SIMPLIFIED_QOI_IMPLEMENTATION in only "one" of your source code files (.c, .cpp .cxx), not your header files
+    Define SIMPLIFIED_QOI_IMPLEMENTATION in only "one"
+    of your source code files (.c, .cpp .cxx), not your header files
     before you include this library to create this implementation
 
     #define SIMPLIFIED_QOI_IMPLEMENTATION
     #include "sQOI.h"
+
+*/
+
+/*
+
+    MIT License
+
+    Copyright (c) 2024-2025 Aftersol
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 
 */
 
