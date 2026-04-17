d982be22cdf8b767e9176b91c42565db24fa7270
Aftersol <42478428+Aftersol@users.noreply.github.com>
2026-04-17 00:22:13 -0700

    Update copyright year in sQOI.h

--- inc/sQOI.h
+++ inc/sQOI.h
@@ -57,7 +57,7 @@
 
     MIT License
 
-    Copyright (c) 2024-2025 Aftersol
+    Copyright (c) 2024-2026 Aftersol
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
